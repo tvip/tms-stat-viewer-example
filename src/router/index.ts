883edfa83544432c0310d805606a793d24eecf44
Egor Danilenko <ed@tvip.ru>
2024-02-19 13:21:32 +0300

    vite BASE_URL used as root path

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,8 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-console.log(process.env.BASE_URL)
 export default router
