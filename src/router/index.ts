cdc4fe688ffb4b09cfeaf7a5cbdf912b58e00062
Egor Danilenko <ed@tvip.ru>
2024-02-19 14:46:44 +0300

    fix build to gh

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,4 +31,5 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import ViteFonts from 'unplugin-fonts/vite'
 // Utilities
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import * as process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -41,4 +42,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: process.env.BASE_URL
 })
